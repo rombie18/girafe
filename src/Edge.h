edb1ed2ea751105b42f670fc22b306fc33390d9c
DaaLuyckx <daan.luyckx@gmail.com>
2022-04-29 14:26:38 +0200

    update Edge class

---
+++ src/Edge.h
@@ -0,0 +1,46 @@
+#include "Vertex.h"
+
+#ifndef SRC_EDGE_H
+#define SRC_EDGE_H
+
+class Edge {
+    private:
+    int ID;
+    Vertex vertex1;
+    Vertex vertex2;
+
+    public:
+    int getID();
+    Vertex getVertex1();
+    Vertex getVertex2();
+
+    int setID(int);
+    Vertex setVertex1(Vertex);
+    Vertex setVertex2(Vertex);
+};
+
+int Edge::getID() {
+    return ID;
+}
+
+Vertex Edge::getVertex1() {
+    return vertex1;
+}
+
+Vertex Edge::getVertex2() {
+    return vertex2;
+}
+
+int Edge::setID(int Id) {
+    ID = Id;
+}
+
+Vertex Edge::setVertex1(Vertex v1) {
+    vertex1 = v1;
+}
+
+Vertex Edge::setVertex1(Vertex v2) {
+    vertex1 = v2;
+}
+
+#endif //SRC_EDGE_H
