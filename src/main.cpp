28c1521194763910463c2d674d5d9f3a484a9f17
Wout Rombouts <woutrombouts18@gmail.com>
2022-05-13 16:16:25 +0200

    try catch

--- src/main.cpp
+++ src/main.cpp
@@ -81,82 +81,88 @@ struct simple_walker: pugi::xml_tree_walker
 
 int main()
 {
-    // Pugi::xml initialization
-    pugi::xml_document file;
-    file.load_file("../../data/graph.graphml");
+    try {
+        // Pugi::xml initialization
+        pugi::xml_document file;
+        file.load_file("../../data/graph.graphml");
 
 
-    // Make 'root' the root of the XML tree.
-    // 'child("graph")' returns the first node which has that name "graph".
-    pugi::xml_node root = file.child("graph"); // 'root' contains a list of graphs. But in reality, there is only one graph. Therefore, let's make this one graph the root.
-
-    
-    // Use 'tree_walker' to step through the .graphml file.
-    // Source: https://pugixml.org/docs/quickstart.html#access
-    simple_walker tree_walker;
-    file.traverse(tree_walker); // Start the XML tree traversal
-
-    // The graph data should now be present in memory in the form of your graph data structure.
+        // Make 'root' the root of the XML tree.
+        // 'child("graph")' returns the first node which has that name "graph".
+        pugi::xml_node root = file.child(
+                "graph"); // 'root' contains a list of graphs. But in reality, there is only one graph. Therefore, let's make this one graph the root.
 
 
-    //cout << "BFS result: " + to_string(bfs(&graph)) + endl;
+        // Use 'tree_walker' to step through the .graphml file.
+        // Source: https://pugixml.org/docs/quickstart.html#access
+        simple_walker tree_walker;
+        file.traverse(tree_walker); // Start the XML tree traversal
 
-    Vertex<SensorNode>* startVertex = nullptr;
-    for(Vertex<SensorNode>* vertex : graph.getVertices()) {
-        if (vertex->getSensorNode()->room == "E116") {
-            startVertex = vertex;
+        // The graph data should now be present in memory in the form of your graph data structure.
+
+
+        //cout << "BFS result: " + to_string(bfs(&graph)) + endl;
+
+        Vertex<SensorNode> *startVertex = nullptr;
+        for (Vertex<SensorNode> *vertex: graph.getVertices()) {
+            if (vertex->getSensorNode()->room == "E116") {
+                startVertex = vertex;
+            }
         }
+
+        list<Edge *> edges = graph.incidentEdges(startVertex);
+
+        bfs(&graph, startVertex);
+
+
+        //for(Vertex* vertex : graph.getVertices()){
+        //    cout << vertex->getSensorNode()->name << endl;
+        //    cout << vertex->getSensorNode()->room << endl;
+        //    cout << vertex->getSensorNode()->temperature << endl;
+        //}
+
+
+        /*
+        Graph graph;
+        SensorNode sn1;
+        SensorNode sn2;
+        sn1.name = "Wout";
+        sn2.name = "Daan";
+        sn1.room = "E110";
+        sn2.room = "E220";
+        sn1.humidity = 0;
+        sn2.humidity = 0;
+        sn1.temperature = 0;
+        sn2.temperature = 0;
+        sn1.co2 = 0;
+        sn2.co2 = 0;
+
+
+        Vertex v1;
+        Vertex v2;
+        v1.setSensorNode(&sn1);
+        v2.setSensorNode(&sn2);
+        v1.setId(1);
+        v2.setId(2);
+
+        Edge edge;
+        edge.setVertex1(&v1);
+        edge.setVertex2(&v2);
+
+        graph.addEdgeToList(&edge);
+        graph.addVertexToList(&v1);
+        graph.addVertexToList(&v2);
+
+        cout << v1.getId() << endl;
+        cout << v2.getId() << endl;
+        cout << sn1.name << endl;
+        cout << sn2.name << endl;
+        */
+
+        return 0;
+
+    } catch (exception e) {
+        cerr << e.what();
+        return 1;
     }
-
-    list<Edge*> edges = graph.incidentEdges(startVertex);
-
-    bfs(&graph, startVertex);
-
-
-    //for(Vertex* vertex : graph.getVertices()){
-    //    cout << vertex->getSensorNode()->name << endl;
-    //    cout << vertex->getSensorNode()->room << endl;
-    //    cout << vertex->getSensorNode()->temperature << endl;
-    //}
-
-
-    /*
-    Graph graph;
-    SensorNode sn1;
-    SensorNode sn2;
-    sn1.name = "Wout";
-    sn2.name = "Daan";
-    sn1.room = "E110";
-    sn2.room = "E220";
-    sn1.humidity = 0;
-    sn2.humidity = 0;
-    sn1.temperature = 0;
-    sn2.temperature = 0;
-    sn1.co2 = 0;
-    sn2.co2 = 0;
-
-
-    Vertex v1;
-    Vertex v2;
-    v1.setSensorNode(&sn1);
-    v2.setSensorNode(&sn2);
-    v1.setId(1);
-    v2.setId(2);
-
-    Edge edge;
-    edge.setVertex1(&v1);
-    edge.setVertex2(&v2);
-
-    graph.addEdgeToList(&edge);
-    graph.addVertexToList(&v1);
-    graph.addVertexToList(&v2);
-
-    cout << v1.getId() << endl;
-    cout << v2.getId() << endl;
-    cout << sn1.name << endl;
-    cout << sn2.name << endl;
-    */
-
-
-    return 0;
 }
